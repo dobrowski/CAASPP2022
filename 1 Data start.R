ae5ba595e30d4bfb24649e2a087ae8589f99052d
David Dobrowski <dobrowski@gmail.com>
2022-08-19 16:53:35 -0700

    Adding graphs

--- 1 Data start.R
+++ 1 Data start.R
@@ -9,6 +9,8 @@ library(here)
 library(ggthemes)
 library(vroom)
 
+library(ggrepel)
+
 
 con <- mcoe_sql_con()
 
@@ -43,7 +45,7 @@ caaspp.mry <- tbl(con, "CAASPP") %>%
 caaspp.mry %>%
     filter(Grade == 13,
            Subgroup_ID == "1",
-           Test_Id == 1, # ELA 
+           Test_Id == 2, # ELA 
            Entity_Type == "District",
            !is.na(Percentage_Standard_Met_and_Above)
     ) %>%
@@ -106,3 +108,201 @@ caaspp.mry %>%
 ggsave(here("figs", paste0("CAASPP ELA Rates Meeting or Exceeding at Salinas Union by Student Group",  Sys.Date(),".png" )),
        width = 8, height = 6)
 
+
+
+
+lolli.subgroups <- function(dist) {
+
+caaspp.mry %>%
+    filter(Grade == 13,
+           str_detect(District_Name,dist),
+           # Subgroup_ID == "1",
+           Test_Id == 1, # ELA 
+           Entity_Type == "District",
+           !is.na(Percentage_Standard_Met_and_Above),
+           !str_detect(Subgroup, " - ")
+    ) %>%
+    lollipop(Percentage_Standard_Met_and_Above,
+             Subgroup,
+             "sea green") +
+    labs(x = "",
+         y = "",
+         color ="",
+         title = paste0("CAASPP ELA Rates Meeting or Exceeding at ",dist," by Student Group"),
+         caption = "Source: Smarter Balance Summative Assessment Research Files  \n https://caaspp-elpac-preview.ets.org/caaspp/ResearchFileListSB") 
+
+}
+
+lolli.subgroups("South Monterey County")
+
+
+lolli.subgroups("Carmel")
+
+
+lolli.schools <- function(dist) {
+    
+    caaspp.mry %>%
+        filter(Grade == 13,
+               str_detect(District_Name,dist),
+               Subgroup_ID == "1",
+               Test_Id == 1, # ELA 
+               Entity_Type == "School",
+               !is.na(Percentage_Standard_Met_and_Above)
+        )%>%
+        lollipop(Percentage_Standard_Met_and_Above,
+                 School_Name,
+                 "sea green") +
+        labs(x = "",
+             y = "",
+             color ="",
+             title = ("CAASPP ELA Rates Meeting or Exceeding by 11th grade"),
+             caption = "Source: Smarter Balance Summative Assessment Research Files  \n https://caaspp-elpac-preview.ets.org/caaspp/ResearchFileListSB") 
+    
+}
+
+lolli.schools("Salinas City")
+
+
+lolli.schools("Alisal")
+
+
+
+###  Slopegraph ------
+
+ent2 <- ent %>%
+    select(-Test_Year, -Type_ID)
+
+
+caaspp.mry2019 <- tbl(con, "CAASPP") %>% 
+    filter(County_Code == "27",
+           # DistrictCode == "10272",
+           Test_Year == "2019") %>%
+    collect() %>%
+    mutate(Subgroup_ID = as.character(Subgroup_ID)) %>%
+    left_join_codebook("CAASPP", "Subgroup_ID") %>%
+    rename(Subgroup = definition) %>%
+    left_join(ent2) %>%
+    mutate(Type_ID = as.character(Type_ID)) %>%
+    left_join_codebook("CAASPP", "Type_ID") %>%
+    rename(Entity_Type = definition) %>%
+    mutate(across(CAASPP_Reported_Enrollment:Area_4_Percentage_Near_Standard, as.numeric))
+
+
+
+caaspp.long <- caaspp.mry %>%
+    bind_rows(caaspp.mry2019) %>%
+    filter(Grade == 13,
+                      Subgroup_ID == "1",
+                      Test_Id == 2, # ELA 
+                      School_Code == "0000000",
+                      # !is.na(Percentage_Standard_Met_and_Above)
+    )
+
+
+
+ggplot(data = caaspp.long, aes(x = Test_Year, y = Percentage_Standard_Met_and_Above, group = District_Name)) +
+    geom_line(aes(color = District_Name, alpha = 1), size = 1) +
+    geom_text_repel(data = caaspp.long %>% filter(Test_Year == "2019"), 
+                    aes(label = District_Name) , 
+                    hjust = "left", 
+                    segment.size = .2,
+                    segment.color = "grey",
+                    size = 3, 
+                    nudge_x = -.4, 
+                    direction = "y") +
+    geom_text_repel(data = caaspp.long %>% filter(Test_Year == "2022"), 
+                    aes(label = District_Name) , 
+                    hjust = "right", 
+                    segment.size = .2,
+                    segment.color = "grey",
+                    fontface = "bold", 
+                    size = 3, 
+                    nudge_x = .4, 
+                    direction = "y") +
+    geom_label(aes(label = Percentage_Standard_Met_and_Above), 
+               size = 2.5, 
+               label.padding = unit(0.05, "lines"), 
+               label.size = 0.0) +
+    theme_hc() +  # Remove the legend
+    theme(axis.text.y      = element_blank()) +
+    theme(panel.grid.major.y = element_blank()) +
+    theme(panel.grid.minor.y = element_blank()) +
+    theme(axis.ticks       = element_blank()) +
+    scale_x_discrete(position = "top") +
+    theme(legend.position = "none") +
+    labs(title = "Most districts decreased\n from 18-19 to 21-22 on Math",
+         y = "Percent Meeting or Exceeding Standard by Grade",
+         x = "")
+
+### Overlapping bars ---
+
+
+caaspp.long <- caaspp.mry %>%
+    bind_rows(caaspp.mry2019) %>%
+    filter(Grade == 13,
+           Subgroup_ID == "1",
+           Test_Id == 2, # ELA 
+           School_Code == "0000000",
+           # !is.na(Percentage_Standard_Met_and_Above)
+    )
+
+
+
+caaspp.long2 <- caaspp.long %>%
+    mutate(ranker = ifelse(Test_Year == "2022", Percentage_Standard_Met_and_Above, NA)) %>%
+    filter(District_Name != "NA",
+           District_Name != "Big Sur Unified")
+
+
+    ggplot(mapping = aes(x = reorder(District_Name, Percentage_Standard_Met_and_Above),
+                         y = Percentage_Standard_Met_and_Above)) +
+        geom_col(data =  caaspp.long2[caaspp.long2$Test_Year == "2019",], 
+                 fill = "light grey",
+                 width = 0.75) +
+        geom_col(data = caaspp.long2[caaspp.long2$Test_Year == "2022",], 
+        #         position = "dodge" ,
+                 width = 0.5,
+                 fill = "dark green") + 
+        coord_flip() + 
+        theme_hc() + 
+        labs(title = "CAASPP Math Percent Meet and Exceed by District",
+             subtitle = "Grey is 2019 and Dark Green is 2022",
+             y = "")
+        
+
+    # ELA version
+    
+    
+    caaspp.long <- caaspp.mry %>%
+        bind_rows(caaspp.mry2019) %>%
+        filter(Grade == 13,
+               Subgroup_ID == "1",
+               Test_Id == 1, # ELA 
+               School_Code == "0000000",
+               # !is.na(Percentage_Standard_Met_and_Above)
+        )
+    
+    
+    
+    caaspp.long2 <- caaspp.long %>%
+        mutate(ranker = ifelse(Test_Year == "2022", Percentage_Standard_Met_and_Above, NA)) %>%
+        filter(District_Name != "NA",
+               District_Name != "Big Sur Unified")
+    
+    
+    ggplot(mapping = aes(x = reorder(District_Name, Percentage_Standard_Met_and_Above),
+                         y = Percentage_Standard_Met_and_Above)) +
+        geom_col(data =  caaspp.long2[caaspp.long2$Test_Year == "2019",], 
+                 fill = "light grey",
+                 width = 0.75) +
+        geom_col(data = caaspp.long2[caaspp.long2$Test_Year == "2022",], 
+                 #         position = "dodge" ,
+                 width = 0.5,
+                 fill = "dark blue") + 
+        coord_flip() + 
+        theme_hc() + 
+        labs(title = "CAASPP ELA Percent Meet and Exceed by District",
+             subtitle = "Grey is 2019 and Dark Blue is 2022",
+             y = "")
+    
+    
